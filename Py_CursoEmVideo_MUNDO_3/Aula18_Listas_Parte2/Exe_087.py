92b67c0382dc924a93ad011500d38fc325e06f7a
arrivabenegustavo <arrivabenegu@gmail.com>
2021-05-04 15:19:14 -0300

    exe088 refeito novamente(aprendendo)

--- Py_CursoEmVideo_MUNDO_3/Aula18_Listas_Parte2/Exe_087.py
+++ Py_CursoEmVideo_MUNDO_3/Aula18_Listas_Parte2/Exe_087.py
@@ -5,27 +5,24 @@ C) O maior valor da segunda linha.'''
 
 matriz = [[0,0,0],[0,0,0],[0,0,0]]
 somapar = somac3 = maior = 0
-# FOR para receber os valores
 for l in range(0, 3):
     for c in range(0, 3):
-        matriz[l][c] = int(input(f'Digite um valor na posição [{l}, {c}]: '))
-# FOR para mostrar os valores recebidos
+        matriz[l][c] = int(input(f'Digite o valor na posição [{l}, {c}]: '))
 for l in range(0, 3):
     for c in range(0, 3):
         print(f'[{matriz[l][c]:^5}]',end=' ')
-        if matriz[l][c] % 2 == 0: # soma os pares dentro da MATRIZ
+        if matriz[l][c] % 2 == 0:
             somapar += matriz[l][c]
-    print()# para quebrar a linha ( 3 linhas com 3 colunas)
-print('-'*30)
-print(f'A soma dos valores pares foi {somapar}')
-for l in range(0, 3): # varre as linhas
-    somac3 += matriz[l][2] # varre as linhas[l] e congela a coluna[2]
-print(f'A soma dos valores da terceira coluna foi {somac3}')
+    print()# Após os laços do FOR c este print quebra pra linha de baixo, e assim até acabar os laços l
+print('-'*30)    
+print(f'A soma dos valores pares foi {somapar}')  
+for l in range(0, 3):
+    somac3 += matriz[l][2]
+print(f'A soma dos valores na terceira coluna foi {somac3}')
 for c in range(0, 3):
     if c == 0 or matriz[1][c] > maior:
         maior = matriz[1][c]
-print(f'O maior valor da segunda linha foi {maior}')
-        
+print(f'O maior valor na segunda linha foi {maior}')
 
 
        
