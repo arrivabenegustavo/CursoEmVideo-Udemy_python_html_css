7b78d0b8f8bba49ee850f48d7182ba8002a94b36
arrivabenegustavo <arrivabenegu@gmail.com>
2021-05-03 21:57:30 -0300

    teste alteração

--- Py_CursoEmVideo_MUNDO_3/Aula18_Listas_Parte2/teste.py
+++ Py_CursoEmVideo_MUNDO_3/Aula18_Listas_Parte2/teste.py
@@ -5,6 +5,6 @@ for c in range(3):
     dados.append(int(input('Idade: ')))
     galera.append(dados[:])
     dados.clear() # exclui o dado pra proxima passagem, senão ele copia o mesmo dado nas outras listas
-print(galera)
+print(galera)# teste comentario
 
    
