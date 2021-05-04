5e54ea6e839c5b299497660ae1aa7808a118ed46
arrivabenegustavo <arrivabenegu@gmail.com>
2021-05-04 08:38:33 -0300

    Refeito do zero para aprendizado

--- Py_CursoEmVideo_MUNDO_3/Aula18_Listas_Parte2/Exe_087.py
+++ Py_CursoEmVideo_MUNDO_3/Aula18_Listas_Parte2/Exe_087.py
@@ -4,28 +4,28 @@ B) A soma dos valores da terceira coluna.
 C) O maior valor da segunda linha.'''
 
 matriz = [[0,0,0],[0,0,0],[0,0,0]]
-soma_par = soma_coluna3 = maior = 0
+somapar = somac3 = maior = 0
+# FOR para receber os valores
 for l in range(0, 3):
     for c in range(0, 3):
-        matriz[l][c] = int(input(f'Digite o número na posição {l}, {c}: '))                                   
+        matriz[l][c] = int(input(f'Digite um valor na posição [{l}, {c}]: '))
+# FOR para mostrar os valores recebidos
 for l in range(0, 3):
     for c in range(0, 3):
         print(f'[{matriz[l][c]:^5}]',end=' ')
-        if matriz[l][c] % 2 == 0:
-            soma_par += matriz[l][c]
-    print()
+        if matriz[l][c] % 2 == 0: # soma os pares dentro da MATRIZ
+            somapar += matriz[l][c]
+    print()# para quebrar a linha ( 3 linhas com 3 colunas)
 print('-'*30)
-print(f'A soma dos números pares foi {soma_par}')
-for l in range(0,3):
-    soma_coluna3 += matriz[l][2] 
-# para somar a 3ª coluna, foi necessário um FOR para varrer as linha e manter a coluna (MATRIZ[l][2]ultima  coluna)
-print(f'A soma dos números da 3ª coluna foi {soma_coluna3}')
-for c in range(0, 3): # Para varrer os valores da coluna e mater a linha
-    # c == 0 -> na primeira contagem MAIOR j recebe o primeiro valor da linha
-    # matriz[1][c] -> começa a comparar se o número dentro de MAIOR é maior que o número na posição da matriz[1][c]
+print(f'A soma dos valores pares foi {somapar}')
+for l in range(0, 3): # varre as linhas
+    somac3 += matriz[l][2] # varre as linhas[l] e congela a coluna[2]
+print(f'A soma dos valores da terceira coluna foi {somac3}')
+for c in range(0, 3):
     if c == 0 or matriz[1][c] > maior:
-        maior = matriz[1][c]          
-print(f'O maior valor da 2ª linha foi {maior}')
+        maior = matriz[1][c]
+print(f'O maior valor da segunda linha foi {maior}')
+        
 
 
        
