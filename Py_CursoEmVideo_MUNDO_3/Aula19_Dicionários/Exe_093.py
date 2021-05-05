6543c9c633da9241efa6ff35f9cd9101d7e28e98
arrivabenegustavo <arrivabenegu@gmail.com>
2021-05-05 18:25:18 -0300

    exe093 melhorias

--- Py_CursoEmVideo_MUNDO_3/Aula19_Dicionários/Exe_093.py
+++ Py_CursoEmVideo_MUNDO_3/Aula19_Dicionários/Exe_093.py
@@ -8,11 +8,9 @@ totgol = 0
 liga['jogador'] = input('Nome do Jogador: ')
 jogos = int(input(f'Quantos jogos {liga["jogador"]} disputou: '))
 for c in range(0, jogos):
-    gol = int(input(f'Marcou quantos gols na partida {c+1}: '))
-    gols.append(gol)
-    totgol += gol 
-    liga['gols'] = gols 
-liga['total'] = totgol
+    gols.append(int(input(f'Marcou quantos gols na partida {c+1}: ')))
+    liga['gols'] = gols
+liga['total'] = sum(gols) # SUM função de soma
 print(30*'-')
 print(liga)
 print(30*'-')
@@ -20,7 +18,6 @@ for k, v in liga.items():
     print(f'O campo {k} tem valor o {v}')
 print(30*'-')
 print(f'O jogador {liga["jogador"]} jogou {jogos} partidas e balançou as redes {liga["total"]} vezes.\n')
-#print()
 for i, v in enumerate(gols):
     print(f'Na {i+1}ª partida marcou {v} vezes.')
        
