41bab2512b773e2e76f4f4fd1a09505489e01e00
arrivabenegustavo <arrivabenegu@gmail.com>
2021-05-12 11:04:08 -0300

    testes usando modulos, funções em outras pastas

---
+++ Py_CursoEmVideo_MUNDO_3/Aula22_Módulos_Pacotes/Módulos/numeros.py
@@ -0,0 +1,6 @@
+import uteis
+n = int(input('Digite um valor: '))
+fat = uteis.fatorial(n)
+print(f'O fatorial de {n} é {fat}.')
+print(f'O dobro de {n} é {uteis.dobro(n)}')
+print(f'O triplo de {n} é {uteis.triplo(n)}')
