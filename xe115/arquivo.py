4b1b024f3685fc527a5f1829c9c98ac5d405d4d3
arrivabenegustavo <arrivabenegu@gmail.com>
2021-05-14 16:23:31 -0300

    exe115b arquivo criado com sucesso

--- xe115/arquivo.py
+++ xe115/arquivo.py
@@ -1,5 +1,8 @@
+from modfuncao import *
+
+
 def arquivoExiste(nome):
-    try:
+    try:# abrir e rt -> ler arquivo de texto
         a = open(nome, 'rt')
         a.close()
     except FileNotFoundError:
@@ -8,11 +11,24 @@ def arquivoExiste(nome):
         return True
     
 def criarArquivo(nome):
-    try:
+    try:# abrir e wt -> escreve um arquivo de texto '+' cria, caso o arquivo não  exista
         a = open(nome, 'wt+')
         a.close()
     except:
         print('Houve um erro na criação do arquivo')
     else:
         print(f'Arquivo {nome} criado com sucesso.')
+        
+        
+def lerArquivo(nome):
+    try:
+        a = open(nome, 'rt')
+    except:
+        print('Não foi possivel abrir o arquivo.')
+    else:
+        cabecalho('PESSOAS CADASTRADAS')
+        print(a.read())
+        
+ 
+        
         
--- xe115/sistema.py
+++ xe115/sistema.py
@@ -1,18 +1,19 @@
+
 from arquivo import *
 from modfuncao import *
 
-arq = 'cursoemvideo.txt'
-if arquivoExiste(arq):
-    print('Arquivo encontrado com sucesso')
-else:
-    print('Arquivo não encontrado')
+arq = 'cursoemvideo.txt' # nome do arquivo
+if not arquivoExiste(arq): # se não tem arquivo
+    criarArquivo(arq) # cria
 
 while True:
     opcao = menu(['Ver pessoas cadastradas', 'Cadastrar nova pessoa', 'Sair do sistema'])
     if opcao == 1:
-        cabecalho('Opção1')
+        # opção de listar o conteúdo de um arquivo.
+        lerArquivo(arq)
     elif opcao == 2:
-        cabecalho('Opção2')
+        # opção de cadastrar novas pessoas
+        pass
     elif opcao == 3:
         cabecalho('Saindo do programa')    
         break
