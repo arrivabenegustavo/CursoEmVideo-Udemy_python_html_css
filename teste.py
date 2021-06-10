34e75716a40b9b59ffb75b3b6beda15537f1cb94
arrivabenegustavo <arrivabenegu@gmail.com>
2021-06-10 18:17:23 -0300

    datetime em portugues

--- teste.py
+++ teste.py
@@ -1,4 +1,9 @@
 from datetime import datetime
+from locale import setlocale, LC_ALL 
+from calendar import mdays # quantos dias tem cada mês do ano
 
+
+setlocale(LC_ALL, '') # localiza onde está sendo executado (neste caso BRasil) coloca a data no idioma português
 atual = datetime.now()
-print(atual)
+formatacao = atual.strftime('%A, %B de %B de %Y')
+print(formatacao)                                
