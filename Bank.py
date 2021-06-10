20632026d16ab88c9155ee30dd1f4da8e9c056c9
arrivabenegustavo <arrivabenegu@gmail.com>
2021-06-10 11:19:17 -0300

    Data Classes

--- Bank.py
+++
@@ -1,5 +0,0 @@
-from people import Cliente
-from contas import Contas
-
-class Banco(Cliente, Contas):
-    
--- perfumaria.py
+++
@@ -1,4 +0,0 @@
-def cabecalho(msg):
-    print('.'*40)
-    print(f'{msg:^40}')
-    print('.'*40)
